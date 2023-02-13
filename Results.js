b76caa510dd77178fb3079ac6a0c16c4f6b52f2a
NoaAharoni <noa.aharoni9@gmail.com>
2023-02-13 12:03:59 +0200

    Initial commit

---
+++ Results.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Results.css';
+
+const Results = (props) => {
+    return (
+        <div className='results-container'>
+
+            {
+                Object.keys(props.results).length > 0 && (
+                    <ul>
+                        <li>Time: {props.results?.time} seconds</li>
+                        <li>Distance: {props.results?.distance} meters</li>
+                        {props.results['excess weight'] && <li>Excess Weight that should be destroyed : {props.results['excess weight']} kg</li>}
+                        {props.results['weather_message'] && <li>{props.results['weather_message']}</li>}
+                    </ul>
+                )
+            }
+        </div>
+    )
+}
+
+
+export default Results;
