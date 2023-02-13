c44f58125d116df005cd20e283531e72ad4b0ec1
NoaAharoni <noa.aharoni9@gmail.com>
2023-02-13 11:37:50 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import MainForm from './MainForm';
+import Results from './Results';
 
-function App() {
+const Form = () => {
+  // State variables to store results from the server
+  const [results, setResults] = useState({});
+  const [error, setError] = useState('');
+
+  // Function to handle form submission
+  const getResults = async (cargoMass, startDate) => {
+    // Clear previous results and error messages
+    setResults({});
+    setError('');
+    try {
+      // Send a post request to the server with user inputs
+      const response = await axios.post('/', { cargo_mass: cargoMass, start_date: startDate });
+      // If the server returns an error, set the error message
+      if (response.data.error) {
+        setError(response.data.error);
+        // If the server returns results, set the results
+      } else {
+        setResults(response.data);
+      }
+    } catch (err) {
+      // If there is a network error, set the error message
+      setError(err.message);
+    }
+  };
+
+  // // Variables to set the date limits for the start date input
+  // const currentDate = new Date();
+  // const startLimit = new Date();
+  // startLimit.setMonth(currentDate.getMonth() - 8); // Set month to be from now until 8 month ago
+  // const endLimit = new Date(currentDate.getTime() + 15 * 24 * 60 * 60 * 1000); // Set the day to be from now to 15 days forward
+
+  // Render the form and the results
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainForm onSubmit={getResults} />
+      {error && <div>Error: {error}</div>}
+      <Results results={results} />
     </div>
   );
-}
+};
 
-export default App;
+export default Form;
